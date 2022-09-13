153342170f2f3a7986460cf0c3f82588eef8878c
Shah Tajbid Tousif <86789646+tajbidtousif@users.noreply.github.com>
2022-09-13 14:08:58 +0600

    Update Variable concept.asm

--- Variable concept.asm
+++ Variable concept.asm
@@ -14,3 +14,17 @@ mian proc
     add mes,48
     mov dl,mes
     int 21h
+        
+    mov ah,1
+    int 21h 
+    mov mes1,al 
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    mov ah,2
+    mov dl,mes1
+    int 21h   
